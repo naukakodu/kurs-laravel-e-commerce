bb59741c8bdaff1f08be7ef4c87d55f4f2a99ffc
ProfesorPerry <jaksza725@gmail.com>
2025-02-18 01:09:25 +0100

    Przygotowanie panelu zarządzania produktami, zmiana nazwy routingu do logowania, do strefy administratora

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/login', [App\Http\Controllers\Administrator\LoginController::class, 'index'])->name('login');
+Route::get('/admin/login', [App\Http\Controllers\Administrator\LoginController::class, 'index'])->name('admin.login.index');
 
 Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', App\Http\Controllers\Administrator\DashboardController::class)->name('dashboard.index');
