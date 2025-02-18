ca47d807934f6b8d33269e86bd35dd44da7667e5
ProfesorPerry <jaksza725@gmail.com>
2025-02-18 01:13:47 +0100

    Przygotowanie panelu zarządzania produktami, właściwa klasa bazowa dla modelu Administrator

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Administrator extends Model
+class Administrator extends Authenticatable
 {
     use HasFactory;
 
