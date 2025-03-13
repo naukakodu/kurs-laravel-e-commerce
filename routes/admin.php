c668821ad94c2c7f02dc50090997b6131a6cf833
ProfesorPerry <jaksza725@gmail.com>
2025-03-13 22:42:46 +0100

    Layout admina

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/admin/login', [App\Http\Controllers\Administrator\LoginController::class, 'index'])->name('admin.login.index');
 Route::post('/admin/login', [App\Http\Controllers\Administrator\LoginController::class, 'store'])->name('admin.login.store');
 
 Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/dashboard', App\Http\Controllers\Administrator\DashboardController::class)->name('dashboard.index');
+    Volt::route('dashboard', 'pages.admin.dashboard.index')->name('dashboard.index');
 });
